567c0881d426773db184da4de47a955923d409ec
simon <simsimguille@gmail.com>
2026-02-22 14:10:22 +0100

    reorganise code and order music by date

--- src/dowloaders/youtube.rs
+++ src/dowloaders/youtube.rs
@@ -1,4 +1,4 @@
-use crate::enums::codec::{self, CODEC_PREFERENCE};
+use crate::enums::codec::{CODEC_PREFERENCE};
 use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::fs;
@@ -6,18 +6,12 @@ use tokio::process::Command;
 use tokio::sync::Semaphore;
 use yt_dlp::Youtube;
 use yt_dlp::client::deps::Libraries;
-use directories;
 
 pub struct YoutubeDownloader {
     output_dir: PathBuf,
     codec_preference: CODEC_PREFERENCE,
     semaphore: Arc<Semaphore>,
     libraries: Arc<Libraries>,
-}
-
-pub struct VideoInfo {
-    pub title: String,
-    pub url: String,
 }
 
 impl YoutubeDownloader {
@@ -48,91 +42,14 @@ impl YoutubeDownloader {
         Ok(())
     }
 
-    pub async fn download_audio_stream_from_url(
-        &self,
-        url: &String,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let _permit = self.semaphore.acquire().await?;
-
-        let output_dir: PathBuf =
-            get_or_create_output_dir(self.output_dir.to_string_lossy().to_string()).await?;
-
-        let fetcher = Youtube::new((*self.libraries).clone(), output_dir).await?;
-
-        let video_infos: yt_dlp::prelude::Video = fetcher.fetch_video_infos(url.clone()).await?;
-
-        let video_title = format!("{}.m4a", video_infos.title);
-
-        let codec_str = &self.codec_preference.to_string();
-
-        if check_if_music_already_exists(&video_infos.title, &fetcher.output_dir, &codec_str).await
-        {
-            println!(
-                "Le fichier {} existe déjà dans le répertoire de sortie. Téléchargement ignoré.",
-                &video_infos.title
-            );
-            return Ok(());
-        }
-
-        println!("Downloading audio for video: {}", &video_title);
-
-        fetcher
-            .download_audio_stream(&video_infos, &video_title)
-            .await?;
-
-        if self.codec_preference != CODEC_PREFERENCE::AAC {
-            let input_file = fetcher.output_dir.join(&video_title);
-            let output_file = fetcher
-                .output_dir
-                .join(format!("{}.{}", video_infos.title, codec_str));
-            let status: std::process::ExitStatus = Command::new(fetcher.libraries.ffmpeg)
-                .arg("-i")
-                .arg(&input_file)
-                .arg("-c:a")
-                .arg(codec_str)
-                .arg("-compression_level")
-                .arg("5")
-                .arg(&output_file)
-                .status()
-                .await?;
-
-            if !status.success() {
-                return Err(format!("Conversion en {} échouée : {}", codec_str, status).into());
-            }
-
-            fs::remove_file(input_file).await?;
-        }
-
-        Ok(())
-    }
-
-    pub async fn get_videos_infos(
-        &self,
-        url: &String,
-    ) -> Result<VideoInfo, Box<dyn std::error::Error>> {
-        let output_dir: PathBuf =
-            get_or_create_output_dir(self.output_dir.to_string_lossy().to_string()).await?;
-
-        let fetcher: Youtube = Youtube::new((*self.libraries).clone(), output_dir).await?;
-
-        let video_infos: yt_dlp::prelude::Video = fetcher.fetch_video_infos(url.clone()).await?;
-
-        Ok(VideoInfo {
-            title: video_infos.title,
-            url: url.clone(),
-        })
-    }
-
     pub async fn dowload_audio_stream_with_progress(
         &self,
         url: &String,
-        callback: impl Fn(u64, u64) + Send + 'static + std::marker::Sync,
+        callback: impl Fn(&str) -> Box<dyn Fn(u64, u64) + Send + 'static + std::marker::Sync>,
     ) -> Result<u64, Box<dyn std::error::Error>> {
         let _permit = self.semaphore.acquire().await?;
 
-        let output_dir: PathBuf =
-            get_or_create_output_dir(self.output_dir.to_string_lossy().to_string()).await?;
-        let fetcher = Youtube::new((*self.libraries).clone(), output_dir).await?;
+        let fetcher = Youtube::new((*self.libraries).clone(), self.output_dir.clone()).await?;
 
         let video_infos: yt_dlp::prelude::Video = fetcher.fetch_video_infos(url.clone()).await?;
         
@@ -152,7 +69,7 @@ impl YoutubeDownloader {
         println!("Downloading audio for video: {}", &video_title);
         
         let download_id = fetcher
-            .download_video_with_progress(&video_infos, &video_title, callback)
+            .download_video_with_progress(&video_infos, &video_title, callback(&video_infos.title))
             .await?;
 
         fetcher.wait_for_download(download_id).await;
@@ -165,14 +82,11 @@ impl YoutubeDownloader {
     pub async fn transform_video_to_audio(
         &self,
         video_title: &str,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let output_dir: PathBuf =
-            get_or_create_output_dir(self.output_dir.to_string_lossy().to_string()).await?;
-        
+    ) -> Result<(), Box<dyn std::error::Error>> {        
         let codec_str = &self.codec_preference.to_string();
-        let video_path = output_dir.join(format!("{}.mp4", video_title));
+        let video_path = self.output_dir.join(format!("{}.mp4", video_title));
         println!("Converting video to audio for: {}", video_path.display());
-        let output_path = output_dir.join(format!("{}.{}", video_title, codec_str));
+        let output_path = self.output_dir.join(format!("{}.{}", video_title, codec_str));
         println!("Output path for audio: {}", output_path.display());
         let status: std::process::ExitStatus = Command::new(&self.libraries.ffmpeg)
             .arg("-i")
@@ -193,27 +107,6 @@ impl YoutubeDownloader {
 
         Ok(())
     }
-}
-
-async fn get_or_create_output_dir(mut path: String) -> Result<PathBuf, Box<dyn std::error::Error>> {    
-    let user_home: PathBuf = match directories::UserDirs::new() {
-        Some(path_home) => path_home.home_dir().to_path_buf(),
-        None => {
-            eprintln!("Impossible de trouver le répertoire utilisateur.");
-            return Err("Impossible de trouver le répertoire utilisateur.".into());
-        }
-    };
-
-    if path.is_empty() {
-        path = String::from("MusicDL");
-    }
-
-    let output_dir = user_home.join(&path);
-    if !output_dir.exists() {
-        fs::create_dir_all(&output_dir).await?;
-    }
-
-    Ok(output_dir)
 }
 
 async fn check_if_music_already_exists(title: &str, output_dir: &PathBuf, codec: &str) -> bool {
--- src/ui/components/download_button.rs
+++ src/ui/components/download_button.rs
@@ -2,6 +2,7 @@ use crate::App;
 use crate::Song;
 use crate::YoutubeDownloader;
 use slint::{ComponentHandle, Model, ModelRc, SharedString, VecModel};
+use std::collections::VecDeque;
 use std::sync::Arc;
 
 pub async fn manage_add_music(app: &App, youtube_downloader: Arc<YoutubeDownloader>) {
@@ -16,65 +17,10 @@ pub async fn manage_add_music(app: &App, youtube_downloader: Arc<YoutubeDownload
             tokio::spawn(async move {
                 let app_handle = app_handle.clone();
                 let youtube_downloader = Arc::clone(&youtube_downloader);
-                let video_info = match youtube_downloader.get_videos_infos(&url).await {
-                    Ok(info) => info,
-                    Err(e) => {
-                        eprintln!("Erreur infos : {}", e);
-                        return;
-                    }
-                };
-
-                let app_handle: Arc<slint::Weak<App>> = Arc::new(app_handle.clone());
-                let video_title: Arc<String> = Arc::new(video_info.title.clone());
 
                 if let Err(e) = youtube_downloader
-                    .dowload_audio_stream_with_progress(&url, move |downloaded: u64, total: u64| {
-                        let app_handle_for_invoke = app_handle.clone();
-                        let title_for_invoke = video_title.clone();
-                        slint::invoke_from_event_loop(move || {
-                            if let Some(app) = app_handle_for_invoke.upgrade() {
-                                let mut songs: Vec<Song> = app.get_songs().iter().collect();
-                                if None
-                                    == songs.iter_mut().find(|s: &&mut Song| {
-                                        s.title == SharedString::from(title_for_invoke.as_str())
-                                    })
-                                {
-                                    println!("Adding song to UI: {}", &title_for_invoke);
-                                    songs.push(Song {
-                                        title: SharedString::from(String::from(
-                                            title_for_invoke.as_str(),
-                                        )),
-                                        download_id: 0,
-                                        total: 100,
-                                        download_progress: 0,
-                                    });
-                                    app.set_songs(ModelRc::new(VecModel::from(songs)));
-                                }
-                            }
-                        })
-                        .unwrap();
-
-                        let app_handle_for_invoke = app_handle.clone();
-                        let title_for_invoke = video_title.clone();
-
-                        slint::invoke_from_event_loop(move || {
-                            if let Some(app) = app_handle_for_invoke.upgrade() {
-                                let mut songs: Vec<Song> = app.get_songs().iter().collect();
-                                if let Some(song) = songs.iter_mut().find(|s: &&mut Song| {
-                                    s.title == SharedString::from(title_for_invoke.as_str())
-                                }) {
-                                    println!("Updating song progress: {}", &song.title);
-                                    song.total = total as i32;
-                                    song.download_progress = (downloaded * 100 / total) as i32;
-                                    println!(
-                                        "Download progress for '{}': {} / {}",
-                                        title_for_invoke, downloaded, total
-                                    );
-                                }
-                                app.set_songs(ModelRc::new(VecModel::from(songs)));
-                            }
-                        })
-                        .unwrap();
+                    .dowload_audio_stream_with_progress(&url, move |video_title: &str| {
+                        Box::new(download_callback(app_handle.clone(), video_title.to_string()))
                     })
                     .await
                 {
@@ -83,4 +29,50 @@ pub async fn manage_add_music(app: &App, youtube_downloader: Arc<YoutubeDownload
             });
         }
     })
+}
+
+pub fn download_callback(app_handle: slint::Weak<App>, video_title: String) -> impl Fn(u64, u64) + Send + 'static + std::marker::Sync {
+    move |downloaded: u64, total: u64| {
+    let app_handle_for_invoke = app_handle.clone();
+    let title_for_invoke = video_title.clone();
+    slint::invoke_from_event_loop(move || {
+        if let Some(app) = app_handle_for_invoke.upgrade() {
+            let mut songs: VecDeque<Song> = app.get_songs().iter().collect();
+            if None
+                == songs.iter_mut().find(|s: &&mut Song| {
+                    s.title == SharedString::from(title_for_invoke.as_str())
+                })
+            {
+                println!("Adding song to UI: {}", &title_for_invoke);
+                songs.push_front(Song {
+                    title: SharedString::from(String::from(
+                        title_for_invoke.as_str(),
+                    )),
+                });
+                app.set_songs(ModelRc::new(VecModel::from(Vec::from(songs))));
+            }
+        }
+    })
+    .unwrap();
+
+    let app_handle_for_invoke = app_handle.clone();
+    let title_for_invoke = video_title.clone();
+
+    slint::invoke_from_event_loop(move || {
+        if let Some(app) = app_handle_for_invoke.upgrade() {
+            let mut songs: Vec<Song> = app.get_songs().iter().collect();
+            if let Some(song) = songs.iter_mut().find(|s: &&mut Song| {
+                s.title == SharedString::from(title_for_invoke.as_str())
+            }) {
+                println!("Updating song progress: {}", &song.title);
+                println!(
+                    "Download progress for '{}': {} / {}",
+                    title_for_invoke, downloaded, total
+                );
+            }
+            app.set_songs(ModelRc::new(VecModel::from(songs)));
+        }
+    })
+    .unwrap();
+    }
 }
