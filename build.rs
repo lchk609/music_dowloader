2d453c4d3ef13dfbc4cb68f00294956a9461983d
simon <simsimguille@gmail.com>
2026-02-23 22:05:16 +0100

    delete materials

--- build.rs
+++ build.rs
@@ -1,13 +1,3 @@
-use std::collections::HashMap;
-use std::path::PathBuf;
-
 fn main() {
-    let mut libs = HashMap::new();
-
-    libs.insert("material".into(), PathBuf::from("material"));
-
-    let config: slint_build::CompilerConfiguration =
-        slint_build::CompilerConfiguration::new().with_library_paths(libs);
-
-    slint_build::compile_with_config("ui/app.slint", config).expect("Slint build failed");
+    slint_build::compile("ui/app.slint").expect("Slint build failed");
 }
