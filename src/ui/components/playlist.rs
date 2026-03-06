70b9322f059870d4149dded412d622845819342b
simon <simsimguille@gmail.com>
2026-03-06 18:22:01 +0100

    add error messages for empty playlist name and url

--- src/ui/components/playlist.rs
+++ src/ui/components/playlist.rs
@@ -1,4 +1,5 @@
-use slint::{ComponentHandle, SharedString, Weak};
+use slint::{ComponentHandle, SharedString, Weak, Model, ModelRc, VecModel};
+use yt_dlp::cache::playlist;
 use std::sync::Arc;
 
 use crate::App;
@@ -23,21 +24,24 @@ impl Playlist {
             app.on_add_playlist({
                 // let youtube_downloader = Arc::clone(&self.youtube_downloader);
                 let config: Config = Config::load().await.unwrap_or_default();
+                let app_weak = app.as_weak();
                 move |playlist_name: SharedString, playlist_url: SharedString| {
                     let mut config = config.clone();
+                    let playlist_for_config = playlist_url.clone().to_string();
                     tokio::spawn(async move {
                         // let youtube_downloader = Arc::clone(&youtube_downloader);
 
                         let _ = config
-                            .update_playlist(playlist_url.to_string(), playlist_name.to_string())
+                            .update_playlist(playlist_url.to_string(), playlist_for_config)
                             .await;
-
-                        println!("Add playlist button clicked");
-                        println!(
-                            "Playlist name: {}, Playlist URL: {}",
-                            playlist_name, playlist_url
-                        );
                     });
+
+                    if let Some(app) = app_weak.upgrade() {
+                        let playlists: slint::ModelRc<SharedString> = app.get_playlists();
+                        let mut playlists_vec: Vec<SharedString> = playlists.iter().collect::<Vec<_>>();
+                        playlists_vec.push(SharedString::from(playlist_name.clone()));
+                        app.set_playlists(ModelRc::new(VecModel::from(playlists_vec)));
+                    }
                 }
             });
         }
